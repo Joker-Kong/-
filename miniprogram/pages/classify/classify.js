06c9327e3197fbac30c4756f051d33fa04e6466c
huangshuang727830 <43749074+huangshuang727830@users.noreply.github.com>
2018-11-30 09:29:25 +0800

    分类

--- miniprogram/pages/classify/classify.js
+++ miniprogram/pages/classify/classify.js
@@ -1,22 +1,14 @@
 const db = wx.cloud.database();
 Page({
   data: {
-    list:[
-      { id:"ly1", name:"护肤专区"},
-      { id: "ly2", name: "彩妆专区" },
-      { id: "ly3", name: "产品系列" },
-      { id: "ly4", name: "肌肤需求" },
-      { id: "ly5", name: "男士专区" },
-      { id: "ly6", name: "黑面膜" },
-      { id: "ly7", name: "肌肤面膜" },
-      { id: "ly8", name: "个人肌肤护理" }
-    ],
     state:0,
     typeData: [],
     contentActive: '1', // 内容栏选中id
     navActive: '', // 导航栏选中id
     heightArr: [],
-    containerH: 0
+    containerH: 0,
+    id:0,
+    classList:[]
   },
   onLoad: function (options) {
     this.getClassify();
@@ -27,33 +19,27 @@ Page({
   getClassify:function(){
     db.collection('classify').get().then(res => {
       console.log(res)
+      this.setData({
+        classList:res.data
+      })
     })
+
   },
   /**
    * 获取各分类产品
    */
   chooseClassify:function(parameter){
-    console.log(parameter)
+    var id = parameter.target.dataset.id
+      
      db.collection('items').where({
-       classify:'护肤'
+       classify: parameter.target.dataset.key
      }).get().then(res =>{
        console.log(res)
+        this.setData({
+          id,
+          flcp:res.data
+        })
      })
-  },
-
-  // chooseClassify: function (parameter) {
-  //   console.log(parameter)
-  //   db.collection('classify').where({
-  //     key: '护肤'
-  //   }).get().then(res => {
-  //     console.log(res)
-  //   })
-  // },
-
-  chooseType(e) {
-    this.setData({
-      state: e.currentTarget.dataset.id
-    })
   }
 })
 
